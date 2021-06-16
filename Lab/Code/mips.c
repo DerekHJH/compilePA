4d9be586f6afc37fa12ba5e3087349dcc6f83c90
DerekHJH <181240020@smail.nju.edu.cn>
2021-06-17 00:09:16 +0800

    finish lab

--- Lab/Code/mips.c
+++ Lab/Code/mips.c
@@ -8,61 +8,45 @@ extern FILE *fp;
 extern int Variable, Function;
 int *param_begin = NULL;
 int *param_size = NULL;
-int *array_size = NULL;
 int *var_begin = NULL;
 int *var_end = NULL;
 int in_which_func = 0;
-void before_funcall()
+void before_funcall(int funNo)
 {
-	fprintf(fp, "addi $sp, $sp, -4\n");
+	fprintf(fp, "move $t0, $sp\n");
+	fprintf(fp, "addi $sp, $sp, -8\n");
+	fprintf(fp, "sw $fp, 4($sp)\n");
 	fprintf(fp, "sw $ra, 0($sp)\n");
+	fprintf(fp, "addi $sp, $sp, -%d\n", (var_end[funNo] - var_begin[funNo]) * 4);
+	fprintf(fp, "move $fp, $sp\n");
+	for(int i = 0; i < param_size[funNo]; i++)
+	{
+		fprintf(fp, "lw $t1, %d($t0)\n", i * 4);
+		fprintf(fp, "sw $t1, %d($fp)\n", i * 4);
+	}
 }
 
-void after_funcall()
+void after_funcall(int funNo)
 {
+	fprintf(fp, "move $sp, $fp\n");	
+	fprintf(fp, "addi $sp, $sp, %d\n", (var_end[funNo] - var_begin[funNo]) * 4);
+	fprintf(fp, "lw $fp, 4($sp)\n");
 	fprintf(fp, "lw $ra, 0($sp)\n");
-	fprintf(fp, "addi $sp, $sp, 4\n");
+	fprintf(fp, "addi $sp, $sp, 8\n");
 }
 void value_load(int reg_no, int var_no)
 {
-	fprintf(fp, "la $t0, _data\n");
-	fprintf(fp, "addi $t0, $t0, %d\n", var_no * 4);
-	if(reg_no != 0)fprintf(fp, "lw $t%d, 0($t0)\n", reg_no);
-	else fprintf(fp, "lw, $v%d, 0($t0)\n", reg_no);
+	var_no -= var_begin[in_which_func];
+	if(reg_no != 0)fprintf(fp, "lw $t%d, %d($fp)\n", reg_no, var_no * 4);
+	else fprintf(fp, "lw, $v%d, %d($fp)\n", reg_no, var_no * 4);
 }
 
 void value_store(int reg_no, int var_no)
 {
-	fprintf(fp, "la $t0, _data\n");
-	fprintf(fp, "addi $t0, $t0, %d\n", var_no * 4);
-	if(reg_no != 0)fprintf(fp, "sw $t%d, 0($t0)\n", reg_no);
-	else fprintf(fp, "sw $v%d, 0($t0)\n", reg_no);
+	var_no -= var_begin[in_which_func];
+	if(reg_no != 0)fprintf(fp, "sw $t%d, %d($fp)\n", reg_no, var_no * 4);
+	else fprintf(fp, "sw $v%d, %d($fp)\n", reg_no, var_no * 4);
 }
-void param_save()
-{
-	int p_begin = param_begin[in_which_func];
-	int p_size = param_size[in_which_func];
-	int p_end = p_begin + p_size;
-	fprintf(fp, "addi $sp, $sp, -%d\n", p_size * 4);
-	for(int i = p_begin; i < p_end; i++)
-	{
-		value_load(1, i);
-		fprintf(fp, "sw $t1, %d($sp)\n", 4 * (i - p_begin));
-	}
-}
-void param_restore()
-{
-	int p_begin = param_begin[in_which_func];
-    int p_size = param_size[in_which_func];
-    int p_end = p_begin + p_size;
-    for(int i = p_begin; i < p_end; i++)
-    {
-    	fprintf(fp, "lw $t1, %d($sp)\n", 4 * (i - p_begin));
-		value_store(1, i);
-    }
-    fprintf(fp, "addi $sp, $sp, %d\n", p_size * 4);
-}
-
 //we only use t1, t2, t3 for ourselves and use t0 for special purpose. when reg_no is 0,it means v0
 void print_mips()
 {
@@ -76,23 +60,26 @@ void print_mips()
 	memset(var_begin, 0, sizeof(int) * (Function + 1));
     memset(var_end, 0, sizeof(int) * (Function + 1));
 
-	array_size = malloc(sizeof(int) * (Function + 1));    
-    memset(array_size, 0, sizeof(int) * (Function + 1));
-
-	fprintf(fp, ".data\n_prompt: .asciiz \"Enter an integer:\"\n_ret: .asciiz \"\\n\"\n_data: .space %d\n.globl main\n.text\nread:\nli $v0, 4\nla $a0, _prompt\nsyscall\nli $v0, 5\nsyscall\njr $ra\n\nwrite:\nli $v0, 1\nsyscall\nli $v0, 4\nla $a0, _ret\nsyscall\nmove $v0, $0\njr $ra\n\n", Variable * 4);
+	fprintf(fp, ".data\n_prompt: .asciiz \"Enter an integer:\"\n_ret: .asciiz \"\\n\"\n.globl main\n.text\nread:\nli $v0, 4\nla $a0, _prompt\nsyscall\nli $v0, 5\nsyscall\njr $ra\n\nwrite:\nli $v0, 1\nsyscall\nli $v0, 4\nla $a0, _ret\nsyscall\nmove $v0, $0\njr $ra\n\n");
 	struct intercode_t *temp = code_head->next;
 	in_which_func = 0;
 	while(temp != code_head)
 	{
 		if(temp->kind == codeFUNCTION)
 		{
-			if(in_which_func != 0)var_end[in_which_func] = temp->op1->value - 1;		
+			if(in_which_func != 0)var_end[in_which_func] = temp->op1->value;		
 			in_which_func = temp->result->value;
 			var_begin[in_which_func] = temp->op1->value;
+			while(temp->next != code_head && temp->next->kind == codePARAM)
+            {
+            	if(param_size[in_which_func] == 0)param_begin[in_which_func] = temp->next->result->value;
+            	param_size[in_which_func]++;
+            	temp = temp->next;
+            }
 		}
 		temp = temp->next;
 	}
-	var_end[in_which_func] = Variable;
+	var_end[in_which_func] = Variable + 1;
 
 	in_which_func = 0;
 	temp = code_head->next;
@@ -103,14 +90,8 @@ void print_mips()
 		{
 			in_which_func = temp->result->value;
 			if(temp->result->value == 1)fprintf(fp, "main:\n");
-			else fprintf(fp, "F%d:\n", temp->result->value);
-			int func_no = temp->result->value;
-			while(temp->next != code_head && temp->next->kind == codePARAM)
-			{
-				if(param_size[func_no] == 0)param_begin[func_no] = temp->next->result->value;
-				param_size[func_no]++;
-				temp = temp->next;
-			}
+			else fprintf(fp, "F%d:\n", temp->result->value);	
+			if(temp->result->value == 1)before_funcall(1);
 		}
 		else if(temp->kind == codeASSIGN)
 		{
@@ -136,9 +117,6 @@ void print_mips()
 		{
 			value_load(1, temp->op1->value);	
             value_store(1, temp->result->value);
-			//fprintf(fp, "la $t1, _data\n");
-            //fprintf(fp, "addi $t1, $t1, %d\n", temp->op1->value * 4);
-			//value_store(1, temp->result->value);
 		}
 		else if(temp->kind == codeRSTAR)
 		{
@@ -166,67 +144,56 @@ void print_mips()
 		}
 		else if(temp->kind == codeRETURN)
 		{
-			fprintf(fp, "addi $sp, $sp, %d\n", array_size[in_which_func]);
+			if(in_which_func == 1)after_funcall(1);
 			value_load(0, temp->result->value);
 			fprintf(fp, "jr $ra\n");
 		}
 		else if(temp->kind == codeDEC)
 		{
-			array_size[in_which_func] += temp->op1->value;
 			fprintf(fp, "addi $sp, $sp, -%d\n", temp->op1->value);
 			fprintf(fp, "move $t1, $sp\n");
 			value_store(1, temp->result->value);
 		}
 		else if(temp->kind == codeARG)
 		{
-			param_save();	
-			struct intercode_t *find_func = temp;
-			while(find_func != code_head && find_func->kind != codeCALL)find_func = find_func->next;
-			int p_size = param_size[find_func->op1->value];
-			int p_begin = param_begin[find_func->op1->value];
-			int p_end = p_begin + p_size - 1;
 			value_load(1, temp->result->value);
-            value_store(1, p_end);
-            p_end--;
+			fprintf(fp, "addi $sp, $sp, -4\n");
+			fprintf(fp, "sw $t1, 0($sp)\n");
 			while(temp->next != code_head && temp->next->kind == codeARG)
 			{
-				assert(p_begin <= p_end);
 				value_load(1, temp->next->result->value);
-				value_store(1, p_end);
-				p_end--;
+				fprintf(fp, "addi $sp, $sp, -4\n");
+                fprintf(fp, "sw $t1, 0($sp)\n");
 				temp = temp->next;
 			}
-			assert(p_begin == p_end + 1);
 		}
 		else if(temp->kind == codeCALL)
 		{
-			before_funcall();
+			before_funcall(temp->op1->value);
 			if(temp->op1->value == 1)fprintf(fp, "jal main\n");
 			else  fprintf(fp, "jal F%d\n", temp->op1->value);
-			after_funcall();
-			param_restore();
+			after_funcall(temp->op1->value);
 			value_store(0, temp->result->value);
 		}
 		else if(temp->kind == codeREAD)
 		{
-			before_funcall();
+			before_funcall(2);
 			fprintf(fp, "jal read\n");
-			after_funcall();
+			after_funcall(2);
 			value_store(0, temp->result->value);
 		}
 		else if(temp->kind == codeWRITE)
 		{
 			value_load(1, temp->result->value);
 			fprintf(fp, "move $a0, $t1\n");
-			before_funcall();
+			before_funcall(3);
             fprintf(fp, "jal write\n");
-            after_funcall();
+            after_funcall(3);
 		}
 		temp = temp->next;
 	}
 	free(param_begin);
 	free(param_size);
-	free(array_size);
 	free(var_begin);
 	free(var_end);
 }
